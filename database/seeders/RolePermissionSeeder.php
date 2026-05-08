d0070a73d5cdf6e0f0dfc32b278329efbb44029d
Prince Kumar <princekumar201926@gmail.com>
2026-05-08 16:11:53 +0530

    feat: add RBAC roles seeding

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        app(PermissionRegistrar::class)->forgetCachedPermissions();
+
+        $roles = [
+            'super-admin',
+            'state-admin',
+            'center-admin',
+            'counsellor',
+            'clerk',
+        ];
+
+        foreach ($roles as $role) {
+            Role::firstOrCreate([
+                'name' => $role,
+                'guard_name' => 'web',
+            ]);
+        }
+    }
+}
